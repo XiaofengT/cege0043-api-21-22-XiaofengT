79d3afc8a337524d2a62cbeb1c2d073344599074
Xiaofeng Tang <zczlxt2@ucl.ac.uk>
2022-02-28 15:40:23 +0000

    Added express file and functionality to support CROSS ORIGIN (CORS) requests

--- dataAPI.js
+++ dataAPI.js
@@ -10,3 +10,10 @@ httpServer.listen(4480);
 app.get('/',function (req,res) {
 res.send("hello world from the Data API");
 });
+// adding functionality to allow cross-origin queries when PhoneGap is running a server
+app.use(function(req, res, next) {
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+next();
+});
