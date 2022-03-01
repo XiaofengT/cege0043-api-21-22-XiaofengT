a524ff2793aedbfc30b8d5d31ceaf2442a03a2a8
Xiaofeng Tang <zczlxt2@ucl.ac.uk>
2022-03-01 18:36:56 +0000

    Modified geoJSON.js

--- routes/geoJSON.js
+++ routes/geoJSON.js
@@ -1,25 +1,28 @@
- var express = require('express');
- var pg = require('pg');
- var geoJSON = require('express').Router();
- var fs = require('fs');
+var express = require('express');
+var pg = require('pg');
+var geoJSON = require('express').Router();
+var fs = require('fs');
 
- // get the username - this will ensure that we can use the same code on multiple machines
- var os = require('os');
- const userInfo = os.userInfo();
- const xiaotang = userInfo.username;
- console.log(username);
- // locate the database login details
- var configtext = ""+fs.readFileSync("/home/"+xiaotang+"/certs/postGISConnection.js"); 
- // now convert the configuration file into the correct format -i.e. a name/value pair array
- var configarray = configtext.split(",");
- var config = {};
- for (var i = 0; i < configarray.length; i++) {
- var split = configarray[i].split(':');
- config[split[0].trim()] = split[1].trim();
- }
- var pool = new pg.Pool(config);
- console.log(config); 
- geoJSON.route('/testGeoJSON').get(function (req,res) {
- res.json({message:req.originalUrl});
- }); 
- module.exports = geoJSON; 
+// get the username - this will ensure that we can use the same code on multiple machines
+var os = require('os');
+const userInfo = os.userInfo();
+const username = userInfo.username;	
+console.log(username);
+// locate the database login details
+var configtext = ""+fs.readFileSync("/home/"+username+"/certs/postGISConnection.js");
+
+// now convert the configruation file into the correct format -i.e. a name/value pair array
+var configarray = configtext.split(",");
+var config = {};
+for (var i = 0; i < configarray.length; i++) {
+	var split = configarray[i].split(':');
+	config[split[0].trim()] = split[1].trim();
+}
+var pool = new pg.Pool(config);
+console.log(config);
+
+geoJSON.route('/testGeoJSON').get(function (req,res) {
+	res.json({message:req.originalUrl});
+});
+
+module.exports = geoJSON; 
